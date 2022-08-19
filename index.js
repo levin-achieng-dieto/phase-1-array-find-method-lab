b4beacfc45eba541406b681dd2bbd8977d8cff1f
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-08-19 14:13:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+const record = [
+    {
+        year: "2015",
+        result: "W"
+    },
+
+    {
+        year: "2016",
+        result: "L"
+    }
+]
+
+
+function superbowlWin(record){
+    let winresult;
+    for(let item of record){
+        if(item.result === "W"){
+            winresult = item["year"];
+            return winresult
+        }
+    }
+}
+
+let denvorsWin = record.find(superbowlWin)
